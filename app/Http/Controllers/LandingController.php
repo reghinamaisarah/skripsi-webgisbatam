d4faa8a34ea06baa49d160070bc055b4d4e1a6c5
reghina22001 <reghina22001@mail.unpad.ac.id>
2026-06-28 19:57:59 +0700

    test

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -2,26 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Bandara;
-use App\Models\Jalan;
-use App\Models\KawasanIndustri;
-use App\Models\Pelabuhan;
-use Illuminate\Http\Request;
-
 class LandingController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
-        $totalKawasan = KawasanIndustri::count();
-        $totalBandara = Bandara::count();
-        $totalPelabuhan = Pelabuhan::count();
-        $totalJalan = Jalan::countMappedRoads();
-
-        return view('landing', compact(
-            'totalKawasan',
-            'totalBandara',
-            'totalPelabuhan',
-            'totalJalan'
-        ));
+        return "OK";
     }
 }
