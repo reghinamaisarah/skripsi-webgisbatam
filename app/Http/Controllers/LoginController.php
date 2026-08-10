778317c265315cabef92263b4b4367b3c6cf4878
reghina22001 <reghina22001@mail.unpad.ac.id>
2026-08-10 19:34:38 +0700

    add

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -24,7 +24,7 @@ class LoginController extends Controller
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
 
-            return redirect('/admin/pageadmin');
+            return redirect('/pageadmin');
         }
 
         return back()
